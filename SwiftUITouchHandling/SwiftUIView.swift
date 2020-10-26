c36793f9e06332449655682970b009a9ba75369f
Pyroh <pyroh@icloud.com>
2020-10-26 17:27:27 +0100

    [IMP] Subclass UIHostingController. [ADD] Button frame retrieval.

--- SwiftUITouchHandling/SwiftUIView.swift
+++ SwiftUITouchHandling/SwiftUIView.swift
@@ -7,23 +7,39 @@
 
 import SwiftUI
 
+
 struct SwiftUIView: View {
+
+    /// Using an optional `CGRect` is a matter of personal preference.
+    private struct ButtonRectPreferenceKey: PreferenceKey {
+        static func reduce(value: inout CGRect?, nextValue: () -> CGRect?) {
+            /// We set it once and for all. It won't change so no need for `onPreferenceChange` to be re-evaluated.
+            value = nextValue() ?? value
+        }
+    }
+    
+    let activeRectBox: CGRectBox
+    
     var body: some View {
-        VStack {
-            Button("SwiftUI Button") {
-                print("SwiftUI tapped")
-            }.border(Color.black)
-        }.frame(minWidth: 0,
-                maxWidth: .infinity,
-                minHeight: 0,
-                maxHeight: .infinity,
-                alignment: .center
-        ).background(Color.red)
+        GeometryReader { proxy in
+            VStack {
+                Button("SwiftUI Button") {
+                    print("SwiftUI tapped")
+                }
+                // Making the button frame bubble up.
+                .anchorPreference(key: ButtonRectPreferenceKey.self, value: Anchor<CGRect>.Source.bounds) { proxy[$0] }
+                .border(Color.black)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.red)
+        }
+        // Catching the button frame.
+        .onPreferenceChange(ButtonRectPreferenceKey.self) { activeRectBox.rect = $0 }
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        SwiftUIView(activeRectBox: .init())
     }
 }
--- SwiftUITouchHandling/ViewController.swift
+++ SwiftUITouchHandling/ViewController.swift
@@ -8,6 +8,43 @@
 import UIKit
 import SwiftUI
 
+
+class CGRectBox {
+    var rect: CGRect?
+}
+
+// Using a subclass is pretty self-explainatory I think.
+class SwiftUIViewHostingController: UIHostingController<SwiftUIView>, UIGestureRecognizerDelegate {
+    /// We can't use a `@Binding` or a `@State` here because we must pass it to `SwiftUIView` during `init`.
+    /// Anyway a binding would make the view's `body` to be recomputed and we don't really want it.
+    let rectBox = CGRectBox()
+    
+    init() {
+        super.init(rootView: SwiftUIView(activeRectBox: rectBox))
+        
+        // SwiftUI (or UIKit ?) set a special gesture recognizer. Let's be its delegate.
+        view.gestureRecognizers?.first?.delegate = self
+    }
+    
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        guard let activeRect = rectBox.rect else { return true }
+        
+        if activeRect.contains(touch.location(in: view)) {
+            print("Tapped on button")
+        } else {
+            print("Tapped on background")
+        }
+        
+        /// Returning `false` may disable the button but the events won't be forwarded to the UIKit button.
+        return true
+    }
+    
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -23,13 +60,10 @@ class ViewController: UIViewController {
         button.layer.borderColor = UIColor.blue.cgColor
         view.addSubview(button)
 
-        let swiftUI = UIHostingController(rootView: SwiftUIView())
+        let swiftUI = SwiftUIViewHostingController()
         swiftUI.view.frame = CGRect(x: 150, y: 150, width: 150, height: 150)
         self.addChild(swiftUI)
         view.addSubview(swiftUI.view)
-        swiftUI.view.isUserInteractionEnabled = false
     }
-
-
 }
 
